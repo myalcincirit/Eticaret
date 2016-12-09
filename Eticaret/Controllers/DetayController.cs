c3eb24164f95c8491dced9322b263c27e663f647
myalcincirit <mcirit@stu.okan.edu.tr>
2016-12-09 03:20:54 +0200

    Detay Sayfası yapıldı

---
+++ Eticaret/Controllers/DetayController.cs
@@ -0,0 +1,23 @@
+﻿using Eticaret.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret.Controllers
+{
+    public class DetayController : Controller
+    {
+        //
+        // GET: /Detay/
+        EticaretContext db = new EticaretContext();
+
+        public ActionResult Index(int id)
+        {
+            var deger = db.Urunlers.Where(x => x.id == id).FirstOrDefault();
+            return View(deger);
+        }
+
+    }
+}
